64d7912ba68cae9b043f27cd3e4067352ede2fc2
Ayomide Onigbinde <onigbindeayomide@gmail.com>
2019-03-03 02:19:06 +0100

    log folder creation now works  🎊🎊

--- index.js
+++ index.js
@@ -1,144 +1,146 @@
-const chalk = require('chalk')
-const fs = require('fs');
-const moment = require('moment');
-const path = require('path')
-const LogPath = (f, p) => path.resolve(__dirname, f, p)
+const chalk = require("chalk");
+const fs = require("fs");
+const moment = require("moment");
+const path = require("path");
+const LogPath = (f, p) => path.resolve(__dirname, f, p);
 
-const fileNames = ['Error', 'Info', 'Debug', 'Silly', 'Warning']
+const fileNames = ["Error", "Info", "Debug", "Silly", "Warning"];
 
-class Utilog {
-  constructor (foldername, makeDefault) {
+class Util {
+  constructor(foldername) {
     this.folderName = foldername
-    this.createDefaultLogFiles = makeDefault
-    this.CreateLogFiles(foldername)
-    this.Print = this.Print.bind(this)
+    this.Print = this.Print.bind(this);
   }
 
-  async MakeFolder(foldername) {
+  async MakeFolder() {
     try {
-      await createFolder(foldername)
+      await createLogFiles(this.folderName);
+      return `done`;
     } catch (e) {
-      throw e
+      throw e;
     }
   }
 
-  async CreateLogFiles(foldername) {
-    await this.MakeFolder(foldername)
-      if (this.createDefaultLogFiles === true) {
-        for (let i = 0; i < fileNames.length; i++) {
-          await fs.createReadStream(LogPath(foldername, `${fileNames[i]}.log`), {flags: 'a+'})
-        }
-      }
-  }
-
-  async Print(level = 'info' , ...message) {
+  async Print(level = "info", ...message) {
     try {
-      let timestamp = moment().format('YYY-MM-DD hh:mm:ss')
-    let a = level.toString().substring(0,1).toUpperCase()
-    let write = fileName => writeFile(fileName, this.folderName, timestamp, message)
+      let timestamp = moment().format("YYY-MM-DD hh:mm:ss");
+      let a = level
+        .toString()
+        .substring(0, 1)
+        .toUpperCase();
+      let write = fileName =>
+        writeFile(fileName, this.folderName, timestamp, message);
 
-    switch(a) {
-      case 'E':
-        write('Error.log')
-        break;
-      case 'I':
-        write('Info.log')
-        break;
-      case 'D':
-        write('Debug.log')
-        break;
-      case 'S':
-        write('Silly.log')
-        break;
-      case 'W':
-        write('Warning.log')
-        break;
-      default:
-        write('Info.log')
-        break;
-    }
+      switch (a) {
+        case "E":
+          write("Error.log");
+          break;
+        case "I":
+          write("Info.log");
+          break;
+        case "D":
+          write("Debug.log");
+          break;
+        case "S":
+          write("Silly.log");
+          break;
+        case "W":
+          write("Warning.log");
+          break;
+        default:
+          write("Info.log");
+          break;
+      }
 
-    let colorise = colorize(level)
-    let pref = prefix(level)
+      let colorise = colorize(level);
+      let pref = prefix(level);
 
-    console.log(colorise(
-      `
+      console.log(
+        colorise(
+          `
       == == == == == == == == == == == == == ==
       ${pref}
       timestamp: ${timestamp}
 
-      LOG DATA: ${JSON.stringify(message, null, 2)}
+      LOG DATA: ${JSON.stringify(message.toString(), null, 2)}
       == == == == == == == == == == == == == ==
       `
-    ))
-
+        )
+      );
     } catch (e) {
-      throw e
+      throw e;
     }
   }
 }
 
-const writeFile = (fileName, folderN, tstamp,...data) => {
+const writeFile = (fileName, folderN, tstamp, ...data) => {
   return new Promise((resolve, reject) => {
-
-    let rt = fs.createWriteStream(LogPath(folderN, fileName), {flags: 'a+'})
-    let dt = {timestamp: tstamp, log: JSON.stringify(...data)}
-    resolve(rt.write(`${JSON.stringify(dt, null, 2)}\n \t \n`))
-
-  })
-}
+    let rt = fs.createWriteStream(LogPath(folderN, fileName), { flags: "a+" });
+    let dt = { timestamp: tstamp, log: JSON.stringify(data.toString()) };
+    resolve(rt.write(`${JSON.stringify(dt, null, 2)}\n \t \n`));
+  });
+};
 
 const colorize = level => {
-  let lowercase = level.toLowerCase()
+  let lowercase = level.toLowerCase();
 
   let colors = {
     error: chalk.bold.red,
     info: chalk.bold.cyan,
     warning: chalk.bold.yellow,
-    debug: chalk.bold.magenta,
-  }
+    debug: chalk.bold.magenta
+  };
 
   switch (lowercase) {
-    case 'error':
-      return colors.error
-    case 'warning':
-      return colors.warning
-    case 'info':
-      return colors.info
-    case 'debug':
-      return colors.debug
+    case "error":
+      return colors.error;
+    case "warning":
+      return colors.warning;
+    case "info":
+      return colors.info;
+    case "debug":
+      return colors.debug;
     default:
-      return colors.info
+      return colors.info;
   }
-}
+};
 
 const prefix = level => {
-  let touppercase = level.toUpperCase()
-    switch (touppercase) {
-      case 'ERROR':
-        return '[ERROR]'
-      case 'WARNING':
-        return '[WARNING]'
-      case 'INFO':
-        return '[INFO]'
-      case 'DEBUG' :
-        return '[DEBUG]'
-      default:
-        return '[NO LEVEL]'
-    }
-}
+  let touppercase = level.toUpperCase();
+  switch (touppercase) {
+    case "ERROR":
+      return "[ERROR]";
+    case "WARNING":
+      return "[WARNING]";
+    case "INFO":
+      return "[INFO]";
+    case "DEBUG":
+      return "[DEBUG]";
+    default:
+      return "[NO LEVEL]";
+  }
+};
 
-const createFolder = foldername => new Promise((resolve, reject) => {
-  fs.stat(path.resolve(__dirname, foldername), err => {
-    if (err && err.code === 'ENOENT') {
-      fs.mkdir(path.resolve(__dirname, foldername), e => {
-        if (e) reject(e.code)
-        else resolve(`Folder created`)
-      })
-    } else {
-      resolve('Folder exists')
-    }
-  })
-})
+const createFolder = foldername =>
+  new Promise((resolve, reject) => {
+    fs.stat(path.resolve(__dirname, foldername), err => {
+      if (err && err.code === "ENOENT") {
+        fs.mkdir(path.resolve(__dirname, foldername), e => {
+          if (e) reject(e.code);
+          else resolve(`Folder created`);
+        });
+      } else {
+        resolve("Folder exists");
+      }
+    });
+  });
 
-module.exports = Utilog
+const createLogFiles = async foldername => {
+  await createFolder(foldername);
+  for (let i = 0; i < fileNames.length; i++) {
+    await fs.createReadStream(LogPath(foldername, `${fileNames[i]}.log`), {
+      flags: "a+"
+    });
+  }
+};
+module.exports = { Util };
